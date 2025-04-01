542f4999cc65e1f97fef403611078214ed17b29d
aloneen <seisenbek2005@mail.ru>
2025-04-01 09:45:01 +0500

    First initial commit

---
+++ src/main/java/kz/seisen/solutionhomework1onspring/SolutionHomework1OnSpringApplication.java
@@ -0,0 +1,13 @@
+package kz.seisen.solutionhomework1onspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionHomework1OnSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolutionHomework1OnSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/seisen/solutionhomework1onspring/SolutionHomework1OnSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.seisen.solutionhomework1onspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionHomework1OnSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
